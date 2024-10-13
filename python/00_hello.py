57d0f318529527abc7c75f86863dc8a0d73f2081
Edwinarias1985 <ariaslopezedwinernesto@gmail.com>
2024-10-12 23:12:27 -0500

    Inicio de curso lenguaje Python con MoureDev youtube

---
+++ python/00_hello.py
@@ -0,0 +1,20 @@
+# Esto es un comentario de solo 1 línea
+'''
+Esto es un 
+comentario 
+con varias 
+líneas. Pues se utiliza 3 comillas simples o 3 comillas dobles tanto para abrir como para cerrar
+'''
+print("Hola mis queridos anacoretas")
+print('Strings se refirer a cadesa de texto' )
+
+x = 5 # Tipo de dato entero positivo (es in Integer)
+x = -5 # Tipo de dato entero negativo (es un integer)
+x = -5,6 # Tipo de dato float negativo (es un float)
+x = 5,6 # Tipo de dato float positivo (es un float)
+x = 'String' # Tipo de dato cadena de caracteres (string)
+x = True # Tipo de dato BOOLEANO (verdadero)
+x = False # Tipo de dato BOOLEANO (falso)
+print(x) # no va con comilla porque es una variable. Si fuera un String si debe ir con comillas.
+# Consultar el tipo de dato
+print(type(5)) # Es de tipo Integer 
